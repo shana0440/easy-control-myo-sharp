e79897fd00fa8f60a80000033ac24d35b6dac49c
郭鎧瑋 <shana0440@gmail.com>
2015-10-10 19:11:50 +0800

    add window class

---
+++ easy-control-c-sharp/common/Window.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy_control_c_sharp.common
+{
+    public class Window
+    {
+        private List<PoseCombination> _poseCollection = new List<PoseCombination>();
+        private string _name;
+
+        public Window(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public void AddPoseCombination(PoseCombination poseCombination)
+        {
+            _poseCollection.Add(poseCombination);
+        }
+
+        public void RemovePoseCombination(PoseCombination poseCombination)
+        {
+            _poseCollection.Remove(poseCombination);
+        }
+
+        public List<PoseCombination> GetPoseCollection()
+        {
+            return _poseCollection;
+        }
+    }
+}
