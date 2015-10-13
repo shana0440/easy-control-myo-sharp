35acbb2b0adcf9b16eca000b3cfe207236fd2e9e
LuJoker <jaker743835@gmail.com>
2015-10-14 00:58:41 +0800

    mode改為window

--- easy-control-c-sharp/form/ModeDetail.cs
+++ easy-control-c-sharp/form/ModeDetail.cs
@@ -6,21 +6,20 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using easy_control_c_sharp.common;
 
 namespace easy_control_c_sharp
 {
     public partial class ModeDetail : Form
     {
         PresentationModel _presentationModel;
-        Mode _mode;
-        public ModeDetail(PresentationModel model, Mode mode)
+        Window _window;
+        public ModeDetail(PresentationModel model, Window window)
         {
             InitializeComponent();
             _presentationModel = model;
-            _mode = mode;
+            _window = window;
             _modePictureBox.Invalidate();
-            BindingSource source = new BindingSource(mode.GetPoseCombinationList(), null);
+            BindingSource source = new BindingSource(window.GetPoseCollection(), null);
             _poseCombinationGridView.DataSource = source;
             InitializeGridView();
         }
@@ -39,33 +38,34 @@ namespace easy_control_c_sharp
             _poseCombinationGridView.Columns.Add(deleteBtn);
             _poseCombinationGridView.Columns["IsEnable"].HeaderText = "enable";
             _poseCombinationGridView.Columns["GetImage"].HeaderText = "icon";
-            
+            _poseCombinationGridView.Columns["IsCompleted"].Visible = false;
+            _poseCombinationGridView.Columns["IsContinue"].Visible = false;
         }
 
         //modePictureBox重畫
         private void ModePictureBoxOnPaint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawImage(_mode.GetImage, 0, 0);
-            e.Graphics.DrawString(_mode.Name, new Font("Arial", 25), Brushes.DarkRed, 60, 8);
+            e.Graphics.DrawImage(_window.GetImage, 0, 0);
+            e.Graphics.DrawString(_window.Name, new Font("Arial", 25), Brushes.DarkRed, 60, 8);
         }
 
         //新增手勢配對button click事件
         private void ClickAddButton(object sender, EventArgs e)
         {
-            PoseCombinationSelect PoseCombinationForm = new PoseCombinationSelect(_presentationModel, _mode);
+            PoseCombinationSelect PoseCombinationForm = new PoseCombinationSelect(_presentationModel, _window);
             PoseCombinationForm.ShowDialog();
         }
 
         //點擊手勢配對GridView事件
         private void PoseCombinationGridViewCellClick(object sender, DataGridViewCellEventArgs e)
         {
-            _presentationModel.ProcessPoseCombinationGridViewCell(_mode, e.RowIndex, e.ColumnIndex, _presentationModel);
+            _presentationModel.ProcessPoseCombinationGridViewCell(_window, e.RowIndex, e.ColumnIndex, _presentationModel);
         }
 
         //關閉視窗時，將此mode的開啟狀態設為false(避免同樣mode的ModeDetail視窗重覆開啟)
         private void ModeDetailFormClosed(object sender, FormClosedEventArgs e)
         {
-            _mode.IsOpen = false;
+            _window.IsOpen = false;
         }
 
         private void PoseCombinationGridViewCellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
