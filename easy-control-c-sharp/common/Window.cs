3e789c22b900860f4045790f6a8de99847e5c2f1
LuJoker <jaker743835@gmail.com>
2015-10-13 20:21:48 +0800

    window補充東西

--- easy-control-c-sharp/common/Window.cs
+++ easy-control-c-sharp/common/Window.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,13 @@ namespace easy_control_c_sharp.common
     {
         private List<PoseCombination> _poseCollection = new List<PoseCombination>();
         private string _name;
+        private bool _enable = true;
+        private bool _open = false;
+        private string _imagePath;
 
         public Window(string name)
         {
             _name = name;
-        }
-
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
         }
 
         public void AddPoseCombination(PoseCombination poseCombination)
@@ -35,6 +33,43 @@ namespace easy_control_c_sharp.common
         public List<PoseCombination> GetPoseCollection()
         {
             return _poseCollection;
+        }
+
+        // image property
+        public void SetImage(string path)
+        {
+            _imagePath = path;
+        }
+
+        // Image property
+        public Bitmap GetImage
+        {
+            get
+            {
+                Bitmap UIBitmap = new Bitmap(Image.FromFile(_imagePath), 50, 50);
+                return UIBitmap;
+            }
+        }
+
+        // Name property
+        public string Name
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+
+        // Enable property
+        public bool IsEnable
+        {
+            set { _enable = value; }
+            get { return _enable; }
+        }
+
+        // Open property
+        public bool IsOpen
+        {
+            set { _open = value; }
+            get { return _open; }
         }
     }
 }
