5ec8c7d431a48e804f9e7deec5f5897a3e2c2679
LuJoker <jaker743835@gmail.com>
2015-10-20 10:27:26 +0800

    change rollUp <-> rollDown

--- easy-control-c-sharp/presentationModel/PresentationModel.cs
+++ easy-control-c-sharp/presentationModel/PresentationModel.cs
@@ -41,8 +41,8 @@ namespace easy_control_c_sharp
             Bitmap source = new Bitmap("Image/arm-device.png");
             List<Tuple<string, Rectangle>> sectionList = new List<Tuple<string, Rectangle>>
             {
-                new Tuple<string, Rectangle>("rollUp", new Rectangle(0, 0, 230, 230)),
-                new Tuple<string, Rectangle>("rollDown", new Rectangle(182, 0, 230, 230)),
+                new Tuple<string, Rectangle>("rollDown", new Rectangle(0, 0, 230, 230)),
+                new Tuple<string, Rectangle>("rollUp", new Rectangle(182, 0, 230, 230)),
                 new Tuple<string, Rectangle>("pitchDown", new Rectangle(0, 182, 230, 230)),
                 new Tuple<string, Rectangle>("pitchUp", new Rectangle(180, 180, 230, 230)),
                 new Tuple<string, Rectangle>("yawUp", new Rectangle(0, 360, 230, 230)),
